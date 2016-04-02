81e2bcb4183cfc6a900edb75978be8a840e49ee8
Rodrigo Villarreal <rodrigogvillarreal@gmail.com>
2016-04-02 12:28:53 -0300

    agrego el admin para la entidad Seccion

---
+++ src/RGV/SiteBundle/Admin/SeccionAdmin.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace RGV\SiteBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class SeccionAdmin extends Admin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('nombre')
+            ->add('slug')
+            ->add('titulo')
+            ->add('descripcion')
+            ->add('activo')
+            ->add('contenido')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('nombre')
+            ->add('slug')
+            ->add('titulo')
+            ->add('descripcion')
+            ->add('activo')
+            ->add('contenido')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('id')
+            ->add('nombre')
+            ->add('slug')
+            ->add('titulo')
+            ->add('descripcion')
+            ->add('activo')
+            ->add('contenido')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('nombre')
+            ->add('slug')
+            ->add('titulo')
+            ->add('descripcion')
+            ->add('activo')
+            ->add('contenido')
+        ;
+    }
+}
