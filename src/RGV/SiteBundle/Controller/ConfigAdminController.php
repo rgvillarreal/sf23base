7053a9d3c34a63a387b84bc951d5cbb170c63d14
Rodrigo Villarreal <rodrigogvillarreal@gmail.com>
2016-04-06 20:16:22 -0300

    agrego la entidad de configuracion de sitios

---
+++ src/RGV/SiteBundle/Controller/ConfigAdminController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace RGV\SiteBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController;
+
+class ConfigAdminController extends CRUDController
+{
+
+}
---
+++ src/RGV/SiteBundle/Entity/Config.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace RGV\SiteBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Config
+ */
+class Config
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     */
+    private $valor;
+
+    /**
+     * @var string
+     */
+    private $tipo;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return Config
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set valor
+     *
+     * @param string $valor
+     * @return Config
+     */
+    public function setValor($valor)
+    {
+        $this->valor = $valor;
+
+        return $this;
+    }
+
+    /**
+     * Get valor
+     *
+     * @return string 
+     */
+    public function getValor()
+    {
+        return $this->valor;
+    }
+
+    /**
+     * Set tipo
+     *
+     * @param string $tipo
+     * @return Config
+     */
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+
+        return $this;
+    }
+
+    /**
+     * Get tipo
+     *
+     * @return string 
+     */
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+}
